4efb7235fa8f3716567edccb9b991320475ff1ed
Hossam Daoud <101238592+HossamDaoud83@users.noreply.github.com>
2023-12-25 09:13:36 +0200

    Update 01 Doctors Evaluation.py

--- pages/01 Doctors Evaluation.py
+++ pages/01 Doctors Evaluation.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(page_title="Lectures Duration", page_icon="16.png")
+st.set_page_config(page_title="GSB", page_icon="16.png")
 st.image("GSB Medium.png", width=120,)
 
-st.title("Doctors Evaluation by Subject")
-components.iframe("https://app.powerbi.com/view?r=eyJrIjoiZGRhNTcxNjgtOWI3YS00N2NhLTg4YjAtOTA5NDlkOTAyYThmIiwidCI6ImE0NzRkYzY0LTQ1ZDEtNDNkOS05N2FjLWM4NWY0ZDMyZmUwMCIsImMiOjl9", width=800, height=600)
+st.title("GSB Doctors Evaluation")
+components.iframe("https://app.powerbi.com/view?r=eyJrIjoiY2RmNDc3NDctOGFmYi00MWFkLWE3YWEtMGQ1YTNhM2FiOWY2IiwidCI6ImE0NzRkYzY0LTQ1ZDEtNDNkOS05N2FjLWM4NWY0ZDMyZmUwMCIsImMiOjl9&embedImagePlaceholder=true", width=800, height=600)
 
 hide_st_style = """
             <style>
